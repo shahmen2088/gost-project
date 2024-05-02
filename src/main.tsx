ae3dde5d42ce761c219b99474249fa9b4a6f92d7
Shokhrukh <shoxrux20888@mail.ru>
2024-05-02 13:30:29 +0300

    fix: lint warn

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import { store } from './app/providers/store/store.ts';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
